70c95306362a5e9db1694a1f2933231006e17957
Magyar Márk <118881797+MagyarMark@users.noreply.github.com>
2025-02-05 12:35:17 +0100

    alap

--- KalandJatek/Form1.Designer.cs
+++ KalandJatek/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 500);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kaland Játék";
---
+++ KalandJatek/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace KalandJatek
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2() 
+        {
+            this.MaximizeBox = false;
+            this.MinimizeBox = true;
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            Image myimage = new Bitmap("main.png");
+            this.BackgroundImage = myimage;
+        }
+
+    }
+}
